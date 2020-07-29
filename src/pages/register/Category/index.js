6222b62941ced84e03c1ca3c802e233cff79039e
Thaina Gomes <tgaraujo022@gmail.com>
2020-07-29 17:56:36 -0300

    Formulário reutilizavel

--- src/pages/register/Category/index.js
+++ src/pages/register/Category/index.js
@@ -1,25 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function RegisterCategory(){
+  const valoresIniciais = {
+    nome:'',
+    descricao: '',
+    cor: '#000'
+  }
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor){
+    // chave: nome, descricao
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(infoEvent){
+    setValue(
+        infoEvent.target.getAttribute('name'), 
+        infoEvent.target.value
+    );
+  }
+    
     return(
       <PageDefault>
-        <h1> 
-            Cadastro de Categoria
-        </h1>
-        <form>
-          <label>
-            Nome da Categoria:
-            <input
-              type="text"
-            />
-          </label>
+        <h1> Cadastro de Categoria: {values.nome} </h1>
+        <form onSubmit={function handleSubmit(infoDoEvento){
+          infoDoEvento.preventDefault();
+          setCategorias([
+            ...categorias,
+            values
+          ]);
+
+          setValues(valoresIniciais);
+        }}>
+          
+          <FormField 
+            label="Nome:"
+            value={values.nome}
+            onChange={handleChange}
+            type="text"
+            name="nome"
+
+          />
+
+          <FormField 
+            label="Descrição:"
+            value={values.descricao}
+            onChange={handleChange}
+            type="text"
+            name="descricao"
+
+          />
+
+          <FormField 
+            label="Cor:"
+            value={values.cor}
+            onChange={handleChange}
+            type="color"
+            name="cor"
+
+          />
 
           <button>
             Cadastrar
           </button>
         </form>
+
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return(
+              <li key={`${categoria}${indice}`}>
+                {categoria.nome}
+              </li>
+            );
+          })}
+        </ul>
+
         <Link to="/">
             Ir para home
         </Link>
